70dac1ee2f86bd75c54f37f9daa430a05979bd6f
tovouk <burningremedy@gmail.com>
2020-06-27 19:40:00 +0000

    show everyones favorite movie

--- src/App.js
+++ src/App.js
@@ -106,6 +106,7 @@ class App extends Component {
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
         <h2>Favorite Movies</h2>
+		{profiles.map(profile => <p>{users[profile.userID].name + "'s favorite movie is " + movies[profile.favoriteMovieID].name +"."}</p>)}
       </div>
     );
   }
